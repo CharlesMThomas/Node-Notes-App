d8615145aa72e03b96dd99e6ddca10b1182b0e22
Charles Thomas <charles@charlesmthomas.com>
2017-06-25 21:43:47 -0400

    Refactor addNote
    Needed to separate repetitive code into own functions for reusability.
    Also needed to provide a console output so users know whether or not
    their new note was successfully added or not.

--- notes.js
+++ notes.js
@@ -2,26 +2,31 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
-const addNote = (title, body) => {
-    let notes = [];
+const fetchNotes = () => {
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
 
+const saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+const addNote = (title, body) => {
+    let notes = fetchNotes();
     let note = {
         title,
         body
     };
-
-    try {
-        let notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);
-    } catch (e) {
-
-    }
-
     let duplicateNotes = notes.filter(note => note.title === title);
 
     if (duplicateNotes.length === 0) {
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
     }
 };
 
