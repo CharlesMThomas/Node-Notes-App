9da71b9dd55ea9d53f0ad5509f95f5c2e01ed1cb
Charles Thomas <charles@charlesmthomas.com>
2017-06-25 21:29:50 -0400

    Create addNote function
    Allows user to add a new note to the file system in JSON format.

--- notes.js
+++ notes.js
@@ -1,7 +1,28 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 const addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    let notes = [];
+
+    let note = {
+        title,
+        body
+    };
+
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    let duplicateNotes = notes.filter(note => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 const getAll = () => {
